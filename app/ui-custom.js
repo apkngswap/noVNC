e6a61c43a1866ff693a5cb58d34a6b83f0a71e8b
moj.moslem <moj.moslem@mataco.local>
2026-02-04 19:16:57 +0330

    refactor(app): :recycle: video quality compatibility
    change ui for sync images with quality

--- app/ui-custom.js
+++ app/ui-custom.js
@@ -18,6 +18,13 @@ UI.connectFinished = function (e) {
     UI.rfb.focus();
 };
 
+// sync images with quality instead video_quality
+const receiveMessageOriginal = UI.receiveMessage;
+UI.receiveMessage = (e) =>
+  e?.data?.action === "setvideoquality"
+    ? (UI.forceSetting("quality", parseInt(e.data.value), false), UI.updateQuality())
+    : receiveMessageOriginal(e);
+
 // Display the desktop name in the document title
 UI.updateDesktopName = function (e) {
     UI.desktopName = e.detail.name;
